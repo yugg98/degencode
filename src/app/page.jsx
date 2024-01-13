d70436afe1d8989ed14317492944a2c431306ab3
Yug Gupta <70101980+yugg98@users.noreply.github.com>
2024-01-13 17:26:04 +0530

    done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,151 +1,104 @@
-"use client"
-import Contactus from "../components/Contact";
-import Services, { OtherSerices } from "../components/Services";
-import { ArrowRightIcon } from "@heroicons/react/24/outline";
-import Image from "next/image";
-import { TypeAnimation } from "react-type-animation";
+import React from "react";
+import Marquee from "react-fast-marquee";
 
-export default function Home() {
+const page = () => {
   return (
-    <div>
-      <div className="md:px-20 px-4 bg-white/10 py-10 pb-20">
-        <p className="md:text-7xl text-5xl py-4 linear-text  font-silkscreen">
-          <TypeAnimation sequence={["Strap in, digital dynamos!"]} repeat={"infinity"}/>
-           
+    <main>
+        <div className="my-10">
+      <Marquee>
+        <p className="font-silkscreen text-6xl mx-4 text-white">Degen Code</p>
+        <p className="text-6xl  text-gray-800 font-bold">
+          Unleashing Tomorrow, Today – Where Innovation Meets the Digital
+          Revolution!
         </p>
-        <p className="text-white mt-4 md:text-2xl text-xl max-w-5xl">
-          At Degencode, we&apos;re slinging blockchain wizardry and crafting
-          decentralised coolness that puts the ordinary to shame.
-        </p>
-        <p className="text-white text-base mt-2 max-w-5xl">
-          Buckle up, because at Degencode, we&apos;re not just riding the wave –
-          we&apos;re turning it into a party. The future has a front-row seat for
-          your fabulousness; are you ready to steal the show?
-        </p>
+      </Marquee>
       </div>
-      <div className="my-20 md:px-10 px-6 max-w-5xl mx-auto">
-        <div className="flex md:flex-nowrap flex-wrap justify-between items-center  gap-x-20  ">
-          <img src="/dev.png" alt="" srcset="" />
-          <div>
-            <p className="md;text-5xl text-3xl md:mt-0 mt-10 text-white">Development Services</p>
-            <p className="text-gray-300 md:text-lg text-sm mt-4">
-              At Degencode, we redefine the digital frontier through our
-              cutting-edge Web3 Tech Development services. Seamlessly blending
-              creativity with technical prowess, we transform visionary concepts
-              into tangible digital realities. From pioneering blockchain
-              solutions to architecting decentralised platforms, we&apos;re not just
-              developers; we&apos;re the architects of the future digital landscape..
+      <div className="flex flex-wrap gap-x-10 max-w-6xl mx-auto px-6 ">
+        <div>
+          <img src="/code.png" alt="" srcset="" />
+        </div>
+        <div className="flex flex-col gap-y-6 md:max-w-[50%] w-full mt-4">
+          <div className="border-b border-gray-600 pb-6">
+            <p className="text-4xl font-silkscreen linear-text2">Incubation</p>
+            <p className="text-md mt-2 text-white">
+              Decade Dive: Incubating and Investing, Capturing a Decade&apos;s
+              Innovation in a Single Leap!
             </p>
           </div>
-        </div>
-        <div className="flex md:flex-nowrap flex-wrap justify-between items-center my-10 gap-x-20  ">
-          <div>
-            <p className="text-gray-300 max-w-lg  md:text-lg text-base mt-4">
-              With a pulse on the latest technologies, we craft bespoke
-              solutions that not only meet but exceed the demands of the Web3
-              era. Elevate your digital presence with Degencode – where
-              innovation meets execution, and the future is now..
+          <div className="border-b border-gray-600 pb-6">
+            <p className="text-4xl font-silkscreen linear-text2">Incubation</p>
+            <p className="text-md mt-2 text-white">
+              Decade Dive: Incubating and Investing, Capturing a Decade&apos;s
+              Innovation in a Single Leap!
             </p>
           </div>
-          <img src="/dev.png" className="md:mt-0 mt-6" alt="" srcset="" />
-        </div>
-      </div>
-      <Services />
-      <div className="bg-white/10 py-20 my-20 px-4 ">
-        <div className="flex md:flex-nowrap  flex-wrap justify-between items-center max-w-5xl mx-auto  gap-x-20  ">
-          <img src="/dev.png" alt="" srcset="" />
-          <div>
-            <p className="md:text-5xl text-3xl md:mt-0 mt-4 text-white">Real World Assets (RWA)</p>
-            <p className="text-gray-300 md:text-lg text-sm mt-4">
-              At Degencode, our Web3 RWA Tokenization services are not just
-              flipping the script; we&apos;re turning it into a blockbuster. We don&apos;t
-              tokenize assets; we transform them into blockchain legends. 
-              Transparency, liquidity, accessibility – they&apos;re not features;
-              they&apos;re our entourage. Why Degencode? It&apos;s where the real world
-              gets a digital makeover.
+          <div className="border-b border-gray-600 pb-6">
+            <p className="text-4xl font-silkscreen linear-text2">Incubation</p>
+            <p className="text-md mt-2 text-white">
+              Decade Dive: Incubating and Investing, Capturing a Decade&apos;s
+              Innovation in a Single Leap!
+            </p>
+          </div>
+          <div className="border-b border-gray-600 pb-6">
+            <p className="text-4xl font-silkscreen linear-text2">Incubation</p>
+            <p className="text-md mt-2 text-white">
+              Decade Dive: Incubating and Investing, Capturing a Decade&apos;s
+              Innovation in a Single Leap!
             </p>
           </div>
         </div>
       </div>
-      <div className=" py-20 my-20 px-4">
-        <div className="flex md:flex-nowrap flex-wrap justify-between items-center max-w-5xl mx-auto  gap-x-20  ">
-          <div>
-            <p className="md:text-5xl text-3xl text-white">Advisory and Consultation</p>
-            <p className="text-gray-300 md:text-lg text-sm mt-4">
-              Our advisory services are a strategic fusion of wisdom and
-              innovation, providing insights that transcend the ordinary. From
-              navigating blockchain landscapes to optimising decentralised
-              ecosystems, we&apos;re the captains of your digital voyage. At
-              Degencode, we&apos;re not just advisors; we&apos;re your partners in
-              pioneering the next era of digital transformation. Elevate your
-              strategy with a touch of Web3 brilliance – where every decision is
-              a leap into the extraordinary.
-            </p>
-          </div>
-          <img src="/dev.png" className="mt-4 md:mt-0" alt="" srcset="" />
+    
+    <div className="my-10 ">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <p className="text-xl font-semibold text-center text-white mb-6">Our Previous Clients</p>
+        <div className="mx-auto mt-6 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+          <img
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            src="https://tailwindui.com/img/logos/158x48/transistor-logo-white.svg"
+
+            alt="Transistor"
+            width={158}
+            height={48}
+          />
+          <img
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            src="https://tailwindui.com/img/logos/158x48/transistor-logo-white.svg"
+
+            alt="Reform"
+            width={158}
+            height={48}
+          />
+          <img
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            src="https://tailwindui.com/img/logos/158x48/transistor-logo-white.svg"
+
+            alt="Tuple"
+            width={158}
+            height={48}
+          />
+          <img
+            className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+            src="https://tailwindui.com/img/logos/158x48/transistor-logo-white.svg"
+
+            alt="SavvyCal"
+            width={158}
+            height={48}
+          />
+          <img
+            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+            src="https://tailwindui.com/img/logos/158x48/transistor-logo-white.svg"
+
+            alt="Statamic"
+            width={158}
+            height={48}
+          />
         </div>
-      </div>
-      <div className="bg-white/10 py-20 my-20 px-4">
-        <div className="flex mb-20 md:flex-nowrap flex-wrap justify-between items-center max-w-5xl mx-auto  gap-x-20  ">
-          <div>
-            <p className="md:text-5xl text-3xl text-white">
-              Token and Smart Contract Services
-            </p>
-            <p className="text-gray-300 md:text-lg text-sm mt-4">
-              At Degencode, our Web3 RWA Tokenization services are not just
-              flipping the script; we&apos;re turning it into a blockbuster. We don&apos;t
-              tokenize assets; we transform them into blockchain legends. 
-              Transparency, liquidity, accessibility – they&apos;re not features;
-              they&apos;re our entourage. Why Degencode? It&apos;s where the real world
-              gets a digital makeover.
-            </p>
-          </div>
-          <img src="/dev.png" className="md:mt-0 mt-4" alt="" srcset="" />
-        </div>
-        <OtherSerices />
-      </div>
-      <div className="my-20 ">
-        <p className="md:text-3xl text-xl md:leading-10 px-5 text-white max-w-6xl text-center mx-auto">
-          Ready to turn your digital dreams into reality? Unleash the power of
-          innovation with Degencode&apos;s Tech Development Services. Whether it&apos;s
-          blockchain brilliance or crafting decentralized wonders, we&apos;ve got the
-          tech prowess to make it happen. Let&apos;s redefine possibilities together.
-          Reach out to us now and let&apos;s build the future you envision! 
-        </p>
-        <button className="mx-auto shadow-lg  hover:bg-white font-semibold my-4 p-4 py-2 gap-x-2  rounded-full bg-transparent border-2 flex border-purple-950 linear-text ">
-          Sign Up Now!
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="23"
-            height="22"
-            viewBox="0 0 23 22"
-            fill="none"
-          >
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M1.71094 10.8754C1.71094 10.693 1.78337 10.5182 1.9123 10.3892C2.04123 10.2603 2.2161 10.1879 2.39844 10.1879H18.6138L14.2867 5.86213C14.1576 5.73304 14.0851 5.55795 14.0851 5.37538C14.0851 5.19281 14.1576 5.01772 14.2867 4.88863C14.4158 4.75954 14.5909 4.68701 14.7734 4.68701C14.956 4.68701 15.1311 4.75954 15.2602 4.88863L20.7602 10.3886C20.8242 10.4525 20.875 10.5284 20.9097 10.6119C20.9443 10.6954 20.9622 10.7849 20.9622 10.8754C20.9622 10.9658 20.9443 11.0554 20.9097 11.1389C20.875 11.2224 20.8242 11.2983 20.7602 11.3621L15.2602 16.8621C15.1311 16.9912 14.956 17.0637 14.7734 17.0637C14.5909 17.0637 14.4158 16.9912 14.2867 16.8621C14.1576 16.733 14.0851 16.5579 14.0851 16.3754C14.0851 16.1928 14.1576 16.0177 14.2867 15.8886L18.6138 11.5629H2.39844C2.2161 11.5629 2.04123 11.4904 1.9123 11.3615C1.78337 11.2326 1.71094 11.0577 1.71094 10.8754Z"
-              fill="url(#paint0_linear_36_4144)"
-            />
-            <defs>
-              <linearGradient
-                id="paint0_linear_36_4144"
-                x1="-0.525344"
-                y1="10.8852"
-                x2="20.9622"
-                y2="10.8615"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop stop-color="#4646FB" />
-                <stop offset="1" stop-color="#BE4DFF" />
-              </linearGradient>
-            </defs>
-          </svg>{" "}
-        </button>
-      </div>
-      <div className="my-10">
-      <Contactus/>
       </div>
     </div>
+
+    </main>
   );
-}
+};
+
+export default page;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,15 @@
 "use client";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
+  { name: "Home", href: "/", current: true },
+  { name: "Technology", href: "/technology", current: false },
+  { name: "Marketing", href: "/marketing", current: false },
+  { name: "MarketMaking", href: "/marketmaking", current: false },
+  { name: "Incubation", href: "/incubation", current: false },
 ];
 
 function classNames(...classes) {
@@ -15,13 +17,14 @@ function classNames(...classes) {
 }
 
 export default function Navbar() {
+  const [current,setCurrent] = useState('/')
   return (
     <Disclosure as="nav" className="bg-white/10">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 py-2">
             <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="absolute -inset-0.5" />
@@ -39,11 +42,12 @@ export default function Navbar() {
               <div className="hidden sm:ml-6 sm:block ">
                 <div className="flex space-x-4">
                   {navigation.map((item) => (
-                    <a
+                    <Link
                       key={item.name}
                       href={item.href}
+                      onClick={()=>setCurrent(item.href)}
                       className={classNames(
-                        item.current
+                        current==item.href
                           ? "bg-gray-900 text-white"
                           : "text-gray-300 hover:bg-gray-700 hover:text-white",
                         "rounded-md px-3 py-2 text-sm font-medium"
@@ -51,17 +55,15 @@ export default function Navbar() {
                       aria-current={item.current ? "page" : undefined}
                     >
                       {item.name}
-                    </a>
+                    </Link>
                   ))}
                 </div>
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <div className="absolute md:flex hidden inset-y-0 right-0  items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <button
                   type="button"
                   className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                 >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
                   <button className="bg-white text-black p-4 py-2 rounded-md">
                     Contact us
                   </button>
@@ -73,12 +75,11 @@ export default function Navbar() {
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
               {navigation.map((item) => (
-                <Disclosure.Button
+                <Link
                   key={item.name}
-                  as="a"
                   href={item.href}
                   className={classNames(
-                    item.current
+                    current==item.href
                       ? "bg-gray-900 text-white"
                       : "text-gray-300 hover:bg-gray-700 hover:text-white",
                     "block rounded-md px-3 py-2 text-base font-medium"
@@ -86,7 +87,7 @@ export default function Navbar() {
                   aria-current={item.current ? "page" : undefined}
                 >
                   {item.name}
-                </Disclosure.Button>
+                </Link>
               ))}
             </div>
           </Disclosure.Panel>
