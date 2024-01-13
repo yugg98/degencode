5c2fa0e9f74f4c2b021c537dce23f363f7c52464
Yug Gupta <70101980+yugg98@users.noreply.github.com>
2024-01-13 20:28:42 +0530

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,42 @@
-"use client";
-import { Fragment, useState } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
-import Link from "next/link";
+/*
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+"use client"; /*
+This example requires some changes to your config:
 
+```
+// tailwind.config.js
+module.exports = {
+  // ...
+  plugins: [
+    // ...
+    require('@tailwindcss/forms'),
+  ],
+}
+```
+*/
+import { Fragment } from "react";
+import { Menu, Popover, Transition } from "@headlessui/react";
+import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+
+const user = {
+  name: "Chelsea Hagon",
+  email: "chelsea.hagon@example.com",
+  imageUrl:
+    "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+};
 const navigation = [
   { name: "Home", href: "/", current: true },
   { name: "Technology", href: "/technology", current: false },
@@ -11,90 +44,97 @@ const navigation = [
   { name: "MarketMaking", href: "/marketmaking", current: false },
   { name: "Incubation", href: "/incubation", current: false },
 ];
-
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-export default function Navbar() {
-  const [current,setCurrent] = useState('/')
+export default function Example() {
   return (
-    <Disclosure as="nav" className="bg-white/10">
-      {({ open }) => (
-        <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 py-2">
-            <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="absolute -inset-0.5" />
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
+    <div className="border border-gray-700 z-20">
+      {/* When the mobile menu is open, add `overflow-hidden` to the `body` element to prevent double scrollbars */}
+      <Popover
+        as="header"
+        className={({ open }) =>
+          classNames(
+            open ? "fixed inset-0 z-40 overflow-y-auto" : "",
+            "bg-black shadow-sm lg:static  lg:overflow-y-visible"
+          )
+        }
+      >
+        {({ open }) => (
+          <>
+            <div className="mx-auto  max-w-7xl px-4 sm:px-6 lg:px-8 py-4">
+              <div className="relative flex justify-between lg:gap-8 xl:grid xl:grid-cols-12">
+                <div className="flex md:absolute md:inset-y-0 md:left-0 lg:static xl:col-span-2">
+                  <div className="flex flex-shrink-0 items-center">
+                    <a href="#">
+                      <img
+                        className="h-10 w-auto"
+                        src="/logo.svg"
+                        alt="Your Company"
+                      />
+                    </a>
+                  </div>
+                </div>
+
+                <div className="flex items-center md:absolute md:inset-y-0 md:right-0 outline-none">
+                  {/* Mobile menu button */}
+                  <Popover.Button className="relative -mx-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                    <span className="absolute -inset-0.5" />
+                    <span className="sr-only">Open menu</span>
+                    {open ? (
+                      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                    ) : (
+                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                    )}
+                  </Popover.Button>
+                </div>
               </div>
-              <div className="flex flex-shrink-0 items-center">
-                <Link href="/">
-                <img className="h-8 w-auto" src="logo.svg" alt="Your Company" />
-                </Link>
-              </div>
-              <div className="hidden sm:ml-6 sm:block ">
-                <div className="flex space-x-4">
+            </div>
+            <Transition
+  as={Fragment}
+  enter="transition ease-out duration-200"
+  enterFrom="opacity-0 translate-x-full" // Enter from left
+  enterTo="opacity-100 translate-x-0"
+  leave="transition ease-in duration-150"
+  leaveFrom="opacity-100 translate-x-0"
+  leaveTo="opacity-0 translate-x-full" // Leave to left
+>
+            <Popover.Panel
+              as="nav"
+              className="absolute border-t border-gray-700 bg-black right-0 md:w-[50vw] w-full h-full z-10"
+              aria-label="Global"
+            >
+              <div className="flex">
+                <div className="border-r h-screen h pt-4">
+                  <Popover.Button>
+                <XMarkIcon className="w-8 h-8 text-white font-semibold mx-4" />
+                </Popover.Button>
+                </div>
+                <div className="mx-auto max-w-3xl w-full space-y-1 px-2 pb-3 pt-2 sm:px-4">
+                  <div className="ml-2 ">
+                    <img src="/logo.svg" alt="" srcset="" />
+                  </div>
                   {navigation.map((item) => (
-                    <Link
+                    <a
                       key={item.name}
                       href={item.href}
-                      onClick={()=>setCurrent(item.href)}
-                      className={classNames(
-                        current==item.href
-                          ? "bg-gray-900 text-white"
-                          : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                        "rounded-md px-3 py-2 text-sm font-medium"
-                      )}
                       aria-current={item.current ? "page" : undefined}
+                      className={classNames(
+                        item.current ? " text-gray-100" : "hover:bg-gray-900",
+                        "block py-2 px-3 border-b text-white text-2xl font-medium"
+                      )}
                     >
                       {item.name}
-                    </Link>
+                    </a>
                   ))}
                 </div>
               </div>
-              <div className="absolute md:flex hidden inset-y-0 right-0  items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <button className="bg-white text-black p-4 py-2 rounded-md">
-                    Contact us
-                  </button>
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
-              {navigation.map((item) => (
-                <Link
-                  key={item.name}
-                  href={item.href}
-                  className={classNames(
-                    current==item.href
-                      ? "bg-gray-900 text-white"
-                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                    "block rounded-md px-3 py-2 text-base font-medium"
-                  )}
-                  aria-current={item.current ? "page" : undefined}
-                >
-                  {item.name}
-                </Link>
-              ))}
-            </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
+            </Popover.Panel>
+            </Transition>
+          </>
+        )}
+      </Popover>
+    </div>
   );
 }
